f0c0753ebe091e093f94a3204711b34f1a726b38
ianroddrs <ianroddrs@gmail.com>
2026-04-05 12:31:38 -0300

    ajuste no background.js ->

--- src/js/background.js
+++ src/js/background.js
@@ -1,21 +1,21 @@
-// Expressão regular para validar a URL (removido o '1' que estava no final causando erro de sintaxe)
 const regexUrl = /^https:\/\/app\.mercos\.com\/\d+\/pedidos\/\d+\/detalhar\/?/;
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && tab.url) {
-        
-        const isPaginaPedido = tab.url && regexUrl.test(tab.url)
+    if (changeInfo.url || changeInfo.status === 'complete') {
+        if (tab.url) {
+            const isPaginaPedido = regexUrl.test(tab.url)
 
-        if (isPaginaPedido) {
-            chrome.action.setIcon({
-                path: { "48": "src/img/icon_active.png" },
-                tabId: tabId
-            });
-        } else {
-            chrome.action.setIcon({
-                path: { "48": "src/img/icon_inactive.png" },
-                tabId: tabId
-            });
+            if (isPaginaPedido) {
+                chrome.action.setIcon({
+                    path: { "48": "/src/img/icon_active.png" },
+                    tabId: tabId
+                }).catch(err => console.error("Erro no ícone ativo:", err));
+            } else {
+                chrome.action.setIcon({
+                    path: { "48": "/src/img/icon_inactive.png" },
+                    tabId: tabId
+                }).catch(err => console.error("Erro no ícone inativo:", err));
+            }
         }
     }
 });
