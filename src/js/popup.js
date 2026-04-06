007d544dc8bc02c4279202ce4ed488bb59a19bca
Ian Rodrigues <ianroddrs@gmail.com>
2026-04-06 14:00:06 -0300

    atualização de layout da extensão

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  const btnGerar = document.getElementById('btnGerar');
+  const extractBtn = document.getElementById('extract-btn');
   const mensagem = document.getElementById('mensagem');
 
   // Obter a aba ativa no momento em que o utilizador clica na extensão
@@ -10,17 +10,17 @@ document.addEventListener('DOMContentLoaded', async () => {
   const regexUrl = /^https:\/\/app\.mercos\.com\/\d+\/pedidos\/\d+\/detalhar\/?/;
 
   if (tab.url && regexUrl.test(tab.url)) {
-    btnGerar.disabled = false;
+    extractBtn.disabled = false;
     mensagem.innerText = "Pronto para exportar os dados deste pedido!";
   } else {
-    btnGerar.disabled = true;
+    extractBtn.disabled = true;
     mensagem.innerText = "Navegue até aos detalhes de um pedido no Mercos para usar a extensão.";
   }
 
   // Ação ao clicar no botão
-  btnGerar.addEventListener('click', async () => {
-    btnGerar.innerText = "Gerando...";
-    btnGerar.disabled = true;
+  extractBtn.addEventListener('click', async () => {
+    extractBtn.innerText = "Gerando...";
+    extractBtn.disabled = true;
     
     try {
       // 1. Injeta a biblioteca SheetJS na página
@@ -35,9 +35,9 @@ document.addEventListener('DOMContentLoaded', async () => {
         files: ['src/js/content.js']
       });
       
-      btnGerar.innerText = "Concluído";
-      btnGerar.style.backgroundColor = "#10b981";
-      btnGerar.style.color = "white";
+      extractBtn.innerText = "Concluído";
+      extractBtn.style.backgroundColor = "#10b981";
+      extractBtn.style.color = "white";
       
       mensagem.innerHTML = "Download iniciado";
     } catch (error) {
